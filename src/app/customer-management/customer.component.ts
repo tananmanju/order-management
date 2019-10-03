a15fada30bdcc296929a576815b4fdeee181da30
Manju Tanan <manju11cs14@gmail.com>
2019-10-04 00:49:28 +0530

    Template changes

--- src/app/customer-management/customer.component.ts
+++ src/app/customer-management/customer.component.ts
@@ -1,46 +1,91 @@
 import { Component } from '@angular/core';
 
 @Component({
-		  selector: 'app',
-		  template: `<section>
-					  <h1>{{title}}</h1>
-					  <ul class="container">
-					  <li class="box" *ngFor="let customer of customers"><h2 class="heading">{{customer.name}}</h2>
-					  <div class="image-grid">
-					  <div><img width=100 height=100 [src]="customer.image"></div>
-					  <div>{{customer.address.city}}</div>
-					  </div>
-					  </li>
-					  </ul>
-		  </section>`,
-		  styles: [`.container{
-						  display: flex; flex-wrap: wrap; list-style: none;}
-                    .box{ background-color: #f1f1f1; width: 300px;
-						  margin: 10px; text-align: center;
-						  line-height:56px;
-						  height:200px;box-shadow: 2px 3px 4px 5px #ae8888;
-}
-				    .heading{
-                          margin: 0px;
-                     background-color: #5656f0;}
-						  .image-grid{
-						      display: flex;
-                              flex-wrap: wrap;
-						  }`]
+	selector: 'app',
+	template: `<section>
+    <h1>{{title}}</h1>
+    <ul class="container">
+        <li *ngFor="let customer of customers" class="box">
+            <h2 class="heading left__padding">{{customer.name}}
+                <span><i aria-hidden="true" class="fa fa-pencil-square-o icon-align"></i></span>
+            </h2>
 
-})
-export class CustomerComponent {
-  title = 'order-management';
-  customers:Array<object>;
+            <div class="image-grid">
+                <figure class="rm-margin"><img [src]="customer.image" class="left__padding" height=100 width=100></figure>
+                <address class="top-padding font-size"><strong>{{customer.address.city}}</strong><br />
+                    <strong>{{customer.address.street}}</strong><br /><br />
+                    <a class="view-order" href="#">View Orders</a>
+                </address>
+            </div>
 
-	constructor(){
-		fetch("http://localhost:5000/api/users/all")
-		.then(response=>response.json())
-		.then(users=>{
-			this.customers = users;
-		})
-		.catch(err=>{
-			console.error(err);
-		})
+        </li>
+    </ul>
+</section>`,
+	styles: [`.container {
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
 	}
-}
+
+	.box {
+	    background-color: #f1f1f1;
+	    width: 300px;
+	    margin: 10px;
+	    height: 200px;
+	    box-shadow: 2px 3px 4px 5px #ae8888;
+	}
+
+	.heading {
+	    margin: 0px;
+	    background-color: #5656f0;
+	    color: #fff;
+	    line-height: 56px;
+	}
+
+	.image-grid {
+	    display: flex;
+	    flex-wrap: wrap;
+	}
+
+	.left__padding {
+	    padding-left: 5px;
+	}
+
+	.icon-align {
+	    float: right;
+	    padding-top: 18px;
+	    padding-right: 5px;
+	}
+
+	.rm-margin {
+	    margin: 10px;
+	}
+
+	.view-order {
+	    text-decoration: none;
+	}
+
+	.top-padding {
+	    padding-top: 10px;
+	}
+
+	.font-size {
+	    font-size: 20px;
+	}`]
+
+		})
+		export class CustomerComponent {
+			title = 'order-management';
+			customers:Array<object>;
+
+			constructor(){
+				fetch("http://localhost:5000/api/users/all")
+				.then(response=>response.json())
+				.then(users=>{
+					this.customers = users;
+				})
+				.catch(err=>{
+					console.error(err);
+				})
+			}
+		}
